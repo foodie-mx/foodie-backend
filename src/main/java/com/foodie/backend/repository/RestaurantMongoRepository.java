5644123cbe9b7f278a704da002cf9a767630841b
Fernando del Pino <fherdelpino@hotmail.com>
2025-08-19 01:38:41 -0600

    refactor restaurant

--- src/main/java/com/foodie/backend/repository/RestaurantMongoRepository.java
+++ src/main/java/com/foodie/backend/repository/RestaurantMongoRepository.java
@@ -3,5 +3,5 @@ package com.foodie.backend.repository;
 import com.foodie.backend.model.Restaurant;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
+public interface RestaurantMongoRepository extends MongoRepository<Restaurant, String> {
 }
