629a52e033142f129a8c405a046577e85b24d36b
Fernando del Pino <fherdelpino@hotmail.com>
2025-08-19 01:45:25 -0600

    refactor restaurants

--- src/main/java/com/foodie/backend/model/Restaurant.java
+++ src/main/java/com/foodie/backend/model/Restaurant.java
@@ -7,7 +7,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document("restaurant")
+@Document("restaurants")
 @Data
 @Builder
 public class Restaurant {
